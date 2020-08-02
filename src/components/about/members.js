d89772d8fcd8dc3f2bd1efb76b8d60161148d89e
TyroneGithub <tyrone.stamaria35@gmail.com>
2020-08-03 00:54:03 +0800

    added members section

---
+++ src/components/about/members.js
@@ -0,0 +1,17 @@
+import React from "react";
+const Members = () => {
+  return (
+    <>
+      <section
+        className="padding-bottom-96 padding-top-64"
+        style={{ background: `#FAFAFC` }}
+      >
+        <div className="wrapper">
+          <h2 className="text-center font-weight-bold">Team Members</h2>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Members;
--- src/pages/about.js
+++ src/pages/about.js
@@ -3,10 +3,12 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import AboutUX from "../components/about/aboutComponent";
+import Members from "../components/about/members";
 const IndexPage = () => (
   <Layout>
     <SEO title="About" />
     <AboutUX />
+    <Members />
   </Layout>
 );
 
