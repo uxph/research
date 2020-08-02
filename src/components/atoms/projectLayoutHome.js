db220cd32a683e18887cba005beb111d662a4693
TyroneGithub <tyrone.stamaria35@gmail.com>
2020-08-02 23:07:28 +0800

    added project data

--- src/components/atoms/projectLayoutHome.js
+++ src/components/atoms/projectLayoutHome.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ProjectLayout = () => {
+const ProjectLayout = ({ title, description, pageLink }) => {
   return (
     <div className="clearfix" style={{ width: `575px` }}>
       <div className="float-left margin-right-24">
@@ -10,14 +10,13 @@ const ProjectLayout = () => {
         />
       </div>
       <div>
-        <h3 className="text-bold" style={{}}>
-          Project S.O.U.P.
-        </h3>
+        <h3 className="font-weight-bold">{title}</h3>
         <p className="margin-y-12" style={{ fontSize: `16px` }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum
-          aliquam sollicitudin
+          {description}
         </p>
-        <button>Read More</button>
+        <a class="button" href={pageLink}>
+          Read More
+        </a>
       </div>
     </div>
   );
