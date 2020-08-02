d22a447d3e40b7e6d8c1050239f63182c5aee8cd
TyroneGithub <tyrone.stamaria35@gmail.com>
2020-08-02 22:55:18 +0800

    added hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,19 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-
+import Hero from "../components/home/hero";
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <header>
+    <Hero />
+    {/* <header>
       <div className="wrapper padding-y-96">
         <h1>UXPH Research Website</h1>
         <a href="/" className="button">
           UXPH Website
         </a>
       </div>
-    </header>
+    </header> */}
   </Layout>
 );
 
